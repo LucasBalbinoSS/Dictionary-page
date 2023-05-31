de84437c0c47cd1ac0d07a33d19ed5d18c51f8e3
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2023-05-31 02:06:10 -0300

    Feat: Tipo de fonte recuperada de forma dinâmica

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,7 @@
         const $fontesBotao = document.querySelector('.js-fontes-botao')
         const $fontesBotaoSeta = document.querySelector('.js-fontes-seta')
         const $fontesModal = document.querySelector('.js-fontes-modal')
+        const $fontesOpcoes = document.querySelectorAll('.js-fontes-opcao')
         const $body = document.querySelector('body')
 
         $fontesBotao.addEventListener('click', () => {
@@ -38,17 +39,25 @@
         }
 
         function fontesTrocar() {
-            const $fontesOpcoes = document.querySelectorAll('.js-fontes-opcao')
+            const $fonteEscolhida = document.querySelector('.js-fonte-escolhida')
+
+            let fonteEscolhidaTipo = $body.getAttribute('data-fonte-tipo')
+            let fonteAtualTipo = $body.getAttribute('data-fonte-tipo')
             let fonteAtual = $body.getAttribute('data-fonte-familia')
 
+            $fonteEscolhida.innerHTML = fonteEscolhidaTipo
             $body.classList.add(`font-['${fonteAtual}']`)
 
             $fontesOpcoes.forEach($fonteOpcao => {
                 $fonteOpcao.addEventListener('click', () => {
                     let fonteEscolhida = $fonteOpcao.getAttribute('data-fonte-familia')
+
+                    fonteEscolhidaTipo = $fonteOpcao.getAttribute('data-fonte-tipo')
                     fonteAtual = $body.getAttribute('data-fonte-familia')
+                    fonteAtualTipo = $body.getAttribute('data-fonte-tipo')
 
                     $body.setAttribute('data-fonte-familia', fonteEscolhida)
+                    $fonteEscolhida.innerHTML = fonteEscolhidaTipo
 
                     $body.classList.remove(`font-['${fonteAtual}']`)
                     $body.classList.add(`font-['${fonteEscolhida}']`)
