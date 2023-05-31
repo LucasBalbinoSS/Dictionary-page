30a42cbc16c55c4df57fb323611db7057250b437
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2023-05-31 00:55:55 -0300

    Feat: Troca dinâmica de fontes

---
+++ src/js/index.js
@@ -0,0 +1,69 @@
+(() => {
+    document.addEventListener('DOMContentLoaded', () => {
+        fontesModal()
+    })
+
+
+    // funcoes
+    const fontesModal = () => {
+        const $fontesBotao = document.querySelector('.js-fontes-botao')
+        const $fontesBotaoSeta = document.querySelector('.js-fontes-seta')
+        const $fontesModal = document.querySelector('.js-fontes-modal')
+        const $body = document.querySelector('body')
+
+        $fontesBotao.addEventListener('click', () => {
+            fontesModalMostrar()
+            fontesModalEsconder()
+        })
+
+        fontesTrocar()
+
+
+        // funcoes internas
+        function fontesModalMostrar() {
+            mostrar($fontesModal)
+            $fontesBotaoSeta.classList.toggle('rotate-180')
+        }
+
+        function fontesModalEsconder() {
+            $body.addEventListener('click', (evento) => {
+
+                if (evento.target.closest('.js-fontes-botao')) {
+                    return
+                }
+
+                esconder($fontesModal)
+                $fontesBotaoSeta.classList.remove('rotate-180')
+            })
+        }
+
+        function fontesTrocar() {
+            const $fontesOpcoes = document.querySelectorAll('.js-fontes-opcao')
+            let fonteAtual = $body.getAttribute('data-fonte')
+
+            $body.classList.add(`font-['${fonteAtual}']`)
+
+            $fontesOpcoes.forEach($fonteOpcao => {
+                $fonteOpcao.addEventListener('click', () => {
+                    let fonteEscolhida = $fonteOpcao.getAttribute('data-fonte')
+                    fonteAtual = $body.getAttribute('data-fonte')
+
+                    $body.setAttribute('data-fonte', fonteEscolhida)
+
+                    $body.classList.remove(`font-['${fonteAtual}']`)
+                    $body.classList.add(`font-['${fonteEscolhida}']`)
+                })
+            })
+        }
+    }
+
+    function esconder($alvo) {
+        $alvo.classList.remove('flex')
+        $alvo.classList.add('hidden')
+    }
+
+    function mostrar($alvo) {
+        $alvo.classList.toggle('hidden')
+        $alvo.classList.toggle('flex')
+    }
+})()
