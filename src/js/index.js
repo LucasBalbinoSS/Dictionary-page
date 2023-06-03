0f06052b864c60ecc83a081b7cc7d8f5c643f3cf
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2023-06-03 03:01:02 -0300

    Style: Aumenta elementos no mobile

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,6 @@
         requisicoesApi()
     })
 
-    console.log('teste')
 
     const $body = document.querySelector('body')
     const $pesquisarCampo = document.querySelector('.js-pesquisar-campo')
@@ -190,8 +189,8 @@
                 `
                     <div>
                         <div class="flex justify-between items-center">
-                            <h2 class="text-3xl font-bold js-titulo-dinamico">${data[0].word}</h2>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7 cursor-pointer hover:scale-110 transition-transform active:scale-95">
+                            <h2 class="text-4xl font-bold js-titulo-dinamico">${data[0].word}</h2>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-9 h-9 cursor-pointer hover:scale-110 transition-transform active:scale-95">
                                 <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 001.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06zM18.584 5.106a.75.75 0 011.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 11-1.06-1.06 8.25 8.25 0 000-11.668.75.75 0 010-1.06z" />
                                 <path d="M15.932 7.757a.75.75 0 011.061 0 6 6 0 010 8.486.75.75 0 01-1.06-1.061 4.5 4.5 0 000-6.364.75.75 0 010-1.06z" />
                             </svg>
@@ -202,11 +201,11 @@
                         </div>
                     </div>
                     <div class="flex flex-col gap-2">
-                        <span class="text-slate-400 text-lg relative flex flex-col after:absolute after:left-20 after:right-0 after:translate-y-1/2 after:top-1/2 after:h-px after:bg-slate-300 after:bg-opacity-70">Meaning</span>
+                        <span class="text-slate-400 text-xl relative flex flex-col after:absolute after:left-20 after:right-0 after:translate-y-1/2 after:top-1/2 after:h-px after:bg-slate-300 after:bg-opacity-70">Meaning</span>
                         <span>${data[0].meanings[0].definitions[0].definition}</span>
                     </div>
                     <div class="flex flex-col gap-2">
-                        <span class="text-slate-400 text-lg relative flex flex-col after:absolute after:left-20 after:right-0 after:translate-y-1/2 after:top-1/2 after:h-px after:bg-slate-300 after:bg-opacity-70">Example</span>
+                        <span class="text-slate-400 text-xl relative flex flex-col after:absolute after:left-20 after:right-0 after:translate-y-1/2 after:top-1/2 after:h-px after:bg-slate-300 after:bg-opacity-70">Example</span>
                         <div class="pl-4 italic relative before:absolute before:h-full before:w-1 before:left-0 before:bg-slate-600">
                             <span>${data[0].meanings[0].definitions[0].example || "<span class=\"text-slate-400 text-lg\">...</span>"}</span>
                         </div>
@@ -260,7 +259,7 @@
                 `
                     <div>
                         <div class="flex justify-between items-center">
-                            <h2 class="text-3xl font-bold js-titulo-dinamico">${palavraPesquisada}</h2>
+                            <h2 class="text-4xl font-bold js-titulo-dinamico">${palavraPesquisada}</h2>
                         </div>
                         <div class="flex flex-col text-slate-400 text-opacity-70">
                             <span>${data[0].partOfSpeech}</span>
@@ -268,7 +267,7 @@
                         </div>
                     </div>
                     <div class="flex flex-col gap-2">
-                        <span class="text-slate-400 text-lg relative flex flex-col after:absolute after:left-28 after:right-0 after:translate-y-1/2 after:top-1/2 after:h-px after:bg-slate-300 after:bg-opacity-70">Significado</span>
+                        <span class="text-slate-400 text-xl relative flex flex-col after:absolute after:left-28 after:right-0 after:translate-y-1/2 after:top-1/2 after:h-px after:bg-slate-300 after:bg-opacity-70">Significado</span>
                         <span>${data[0].meanings[0]}</span>
                     </div>
                 `
